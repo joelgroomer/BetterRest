044f54959d5ddbf781198846c4e4ce3f56b636a4
Joel Groomer <joelgroomer@gmail.com>
2021-06-08 22:02:36 -0700

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Joel Groomer on 6/8/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
