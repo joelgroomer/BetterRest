0d8a682f8ea3f4620934ab37861fa3e7f8a284f5
Joel Groomer <joelgroomer@gmail.com>
2021-06-11 15:23:35 -0700

    Challenge 3: Removed alert. Optimal bedtime now displays constantly at the bottom of the screen.

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -11,11 +11,7 @@ struct ContentView: View {
     @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
-    
-    @State private var alertTitle = ""
-    @State private var alertMessage = ""
-    @State private var showingAlert = false
-    
+        
     static var defaultWakeTime: Date {
         var components = DateComponents()
         components.hour = 7
@@ -25,44 +21,45 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-            Form {
-                Section(header: Text("When do you want to wake up?")) {
-                    DatePicker("Please enter a time",
-                               selection: $wakeUp,
-                               displayedComponents: .hourAndMinute)
-                }
-                
-                Section(header: Text("Desired amount of sleep")) {
-                    Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
-                        Text("\(sleepAmount, specifier: "%g") hours")
+            VStack(alignment: .center) {
+                Form {
+                    Section(header: Text("When do you want to wake up?")) {
+                        DatePicker("Please enter a time",
+                                   selection: $wakeUp,
+                                   displayedComponents: .hourAndMinute)
                     }
-                }
-                
-                Section(header: Text("Daily coffee intake")) {
-                    Picker("Number of cups", selection: $coffeeAmount) {
-                        ForEach(1...20, id: \.self) { cups in
-                            if cups == 1 {
-                                Text("1 cup")
-                            } else {
-                                Text("\(cups) cups")
+                    
+                    Section(header: Text("Desired amount of sleep")) {
+                        Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+                            Text("\(sleepAmount, specifier: "%g") hours")
+                        }
+                    }
+                    
+                    Section(header: Text("Daily coffee intake")) {
+                        Picker("Number of cups", selection: $coffeeAmount) {
+                            ForEach(1...20, id: \.self) { cups in
+                                if cups == 1 {
+                                    Text("1 cup")
+                                } else {
+                                    Text("\(cups) cups")
+                                }
                             }
                         }
                     }
                 }
+                
+                Text("Optimal bedtime:")
+                    .font(.title)
+                Text("\(calculateBedtime())")
+                    .font(.largeTitle)
+                Spacer()
             }
+            
             .navigationBarTitle("BetterRest")
-            .navigationBarItems(trailing:
-                                    Button(action: calculateBedtime) {
-                                        Text("Calculate")
-                                    }
-            )
-            .alert(isPresented: $showingAlert, content: {
-                Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
-            })
         }
     }
     
-    func calculateBedtime() {
+    func calculateBedtime() -> String {
         let model = SleepCalculator()
         
         let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
@@ -76,15 +73,10 @@ struct ContentView: View {
             
             let formatter = DateFormatter()
             formatter.timeStyle = .short
-            alertTitle = "Your ideal bedtime is..."
-            alertMessage = formatter.string(from: sleepTime)
+            return formatter.string(from: sleepTime)
         } catch {
-            alertTitle = "Error"
-            alertMessage = "Sorry, there was a problem calculating your bedtime."
+            return "ERR"
         }
-        
-        showingAlert = true
-        
     }
 }
 
